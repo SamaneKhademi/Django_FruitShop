59a803bf90aff600a298360e539efe522919e514
Samane Khademi <s.khademi1992@gmail.com>
2024-08-13 17:23:14 +0330

    create models

--- store/admin.py
+++ store/admin.py
@@ -1,4 +1,25 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
+from .models import Category, Customer, Product, Order
 
 admin.site.unregister(Group)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'phone', 'email', 'password')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'category', 'description', 'image', 'is_sale', 'sale_price')
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product', 'customer', 'quantity', 'address', 'phone', 'date', 'status')
