169035b5fb1497a03faeb03fdbaac37a8369d8f8
Samane Khademi <s.khademi1992@gmail.com>
2024-08-15 02:02:57 +0330

    user profile

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
-from .models import Category, Customer, Product, Order
+from .models import Category, Customer, Product, Order, Profile
+from django.contrib.auth.models import User
 
 admin.site.unregister(Group)
 
@@ -23,3 +24,27 @@ class ProductAdmin(admin.ModelAdmin):
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('id', 'product', 'customer', 'quantity', 'address', 'phone', 'date', 'status')
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'date_modified', 'phone', 'state', 'city', 'address', 'zipcode', 'old_cart')
+
+
+# Mix profile info and user info
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+
+# Extend User model
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    field = ['username', 'first_name', 'last_name', 'email']
+    inlines = [ProfileInline]
+
+
+# Unregister the old way
+admin.site.unregister(User)
+
+# Re_register the new way
+admin.site.register(User, UserAdmin)
