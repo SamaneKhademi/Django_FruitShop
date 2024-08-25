936767e66ecd92a49f418a2d22390f56672d197f
Samane Khademi <s.khademi1992@gmail.com>
2024-08-26 00:23:32 +0330

    customer order model and order item model

--- payment/admin.py
+++ payment/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import ShippingAddress
+from .models import ShippingAddress, Order, OrderItem
 
 
 @admin.register(ShippingAddress)
@@ -7,3 +7,11 @@ class ShippingAddressAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'shipping_full_name', 'shipping_email', 'shipping_state', 'shipping_city', 'shipping_address',  'shipping_zipcode')
 
 
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'full_name', 'email', 'shipping_address', 'amount_paid', 'date_ordered')
+
+
+@admin.register(OrderItem)
+class OrderItemsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'order', 'product', 'user', 'quantity', 'price')
